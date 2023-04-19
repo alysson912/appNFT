9c36c651a010a69134c93c0835f80222610efb57
Alysson Menezes <almenezes912@gmail.com>
2023-04-19 18:32:39 -0300

    criando elementos, adicionando elementos a  screen, criando codeSnipets.

--- appNFT/appNFT/src /Feature/Home/Screen/HomeScreen.swift
+++ appNFT/appNFT/src /Feature/Home/Screen/HomeScreen.swift
@@ -10,7 +10,7 @@ import UIKit
 class HomeScreen: UIView {
 
     
-    lazy var view: UIView = {
+    lazy var viewBackGround: UIView = {
        let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
@@ -21,17 +21,62 @@ class HomeScreen: UIView {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
         image.image = UIImage( named: "vector" )
-       // image.contentMode = .scaleAspectFit
-        //image.backgroundColor = .red
         return image
+    }()
+    
+    lazy var searchBar: UISearchBar = {
+       let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.clipsToBounds = true
+        searchBar.layer.cornerRadius = 20
+        searchBar.placeholder = "Pesquise pelo nome:"
+        return searchBar
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+       let collectionView = UICollectionView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        // TO DO: Register
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)// a collection nasce com a cell contendo a distancias respectivas
+        layout.scrollDirection = .horizontal
+        collectionView.setCollectionViewLayout(layout, animated: false)
+        
+        return collectionView
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none // retirando linhas
+        tableView.showsVerticalScrollIndicator = false // desativando scrool indicator
+        // TO DO: Register
+        tableView.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        return tableView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addViews()
+        setupConstraints()
     }
     
+    private func addViews(){
+        addSubview(viewBackGround)
+        viewBackGround.addSubview(logoImageView)
+        viewBackGround.addSubview(searchBar)
+        viewBackGround.addSubview(collectionView)
+        addSubview(tableView)
+    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+    private func setupConstraints(){
+        NSLayoutConstraint.activate([
+        
+        
+        ])
+    }
 }
